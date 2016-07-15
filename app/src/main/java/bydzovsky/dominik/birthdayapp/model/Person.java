3daab09920cc0df0e20c6b9486bf93166330a0c4
ee5415 <ee5415>
2016-07-15 11:54:28 +0800

    Initial commit

---
+++ app/src/main/java/bydzovsky/dominik/birthdayapp/model/Person.java
@@ -0,0 +1,15 @@
+package bydzovsky.dominik.birthdayapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by ee5415 on 15.7.2016.
+ */
+public class Person {
+    String name;
+    String surname;
+    Date dateOfBirth;
+
+
+
+}
---
+++ app/src/main/java/bydzovsky/dominik/birthdayapp/utility/Service.java
@@ -0,0 +1,9 @@
+package bydzovsky.dominik.birthdayapp.utility;
+
+/**
+ * Created by ee5415 on 15.7.2016.
+ */
+public class Service {
+
+
+}
