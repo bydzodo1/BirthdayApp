c52041f7df6d84a971b843868fb531c132946e25
ee5415 <dominik01@email.cz>
2016-07-18 10:59:46 +0800

    ..

--- app/src/main/java/bydzovsky/dominik/birthdayapp/utility/Service.java
+++ app/src/main/java/bydzovsky/dominik/birthdayapp/utility/Service.java
@@ -5,15 +5,24 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.List;
 
 import bydzovsky.dominik.birthdayapp.model.Person;
+import bydzovsky.dominik.birthdayapp.model.SQLiteDataManager;
 
 /**
  * Created by ee5415 on 15.7.2016.
  */
 public class Service {
+    SQLiteDatabase myDatabase;
 
-
+    public Service() {
+        if (null==myDatabase) {
+            myDatabase = new SQLiteDataManager().getMyDatabase();
+        }
+    }
     public List<Person> getListOfPeople(){
-        return null;
+        String query = "SELECT * FROM " + SQLiteDataManager.CONTACTS_TABLE_NAME + "order by "+SQLiteDataManager.CONTACTS_COLUMN_BIRTHDAY+";";
+
+
+        return
     }
 
 }
